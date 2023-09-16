20fb150fd7f39ede74aafd07727190c7ce1978cb
shahid <razashahid532@gmail.com>
2023-09-17 00:21:00 +0530

    Added Swagger and yml

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 const express = require('express');
 const { ServerConfig } = require('./config');
-
 const app = express();
+
+/**
+ * swagger >
+ */
+
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yml');
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+/**
+ * swagger <
+ */
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
