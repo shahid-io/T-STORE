16a34e673229f052dcd619ac3009ba090113b761
SupeerSanny <ksanny556@gmail.com>
2023-09-20 22:39:40 +0530

    changes

--- src/routes/v1/user.routes.js
+++ src/routes/v1/user.routes.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const { UserController } = require('../../controllers');
+const express = require("express");
+const { UserController } = require("../../controllers");
 const router = express.Router();
 
 /**
  * /api/v1/user
  */
 
-router.post('signup/', UserController.signup);
-router.post('/login', UserController.login);
+router.post("/signup", UserController.signup);
+router.post("/login", UserController.login);
 
 module.exports = router;
