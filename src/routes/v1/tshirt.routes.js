f3cba25575c9fbb60a24db14e66f272a0d0c3525
SupeerSanny <ksanny556@gmail.com>
2023-09-22 15:21:40 +0530

    remove tokenAuth from getTshirt

--- src/routes/v1/tshirt.routes.js
+++ src/routes/v1/tshirt.routes.js
@@ -1,13 +1,17 @@
-const express = require('express');
-const { TshirtController } = require('../../controllers');
-const { AuthMiddleware } = require('../../middlewares');
+const express = require("express");
+const { TshirtController } = require("../../controllers");
+const { AuthMiddleware } = require("../../middlewares");
 const router = express.Router();
 
 /**
  * /api/v1/user
  */
 
-router.post('/', AuthMiddleware.verifyToken, TshirtController.addTshirt);
-router.get('/', AuthMiddleware.verifyToken, TshirtController.getTshirts);
-router.get('/category', AuthMiddleware.verifyToken, TshirtController.getAllTshirtsWithCategoryNames);
+router.post("/", AuthMiddleware.verifyToken, TshirtController.addTshirt);
+router.get("/", TshirtController.getTshirts);
+router.get(
+  "/category",
+  AuthMiddleware.verifyToken,
+  TshirtController.getAllTshirtsWithCategoryNames
+);
 module.exports = router;
