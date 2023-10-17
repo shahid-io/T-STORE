5a4432e990caff48950c0659a161bae9c82b13f4
SupeerSanny <ksanny556@gmail.com>
2023-10-17 17:06:23 +0530

    remove netlify fn

--- src/config/database.config.js
+++ src/config/database.config.js
@@ -1,20 +1,20 @@
 const mongoose = require("mongoose");
-const { MONGO_URL } = process.env
+const { MONGO_URL } = process.env;
 
 async function connect() {
-    mongoose
-        .connect(MONGO_URL, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        })
-        .then(() => {
-            console.log("DB CONNECTION ESHTABLISH");
-        })
-        .catch((error) => {
-            console.log("DB CONNECTION FAILED");
-            console.log(error);
-            process.exit(1);
-        });
-};
+  mongoose
+    .connect(MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log("DB CONNECTION ESHTABLISH");
+    })
+    .catch((error) => {
+      console.log("DB CONNECTION FAILED");
+      console.log(error);
+      process.exit(1);
+    });
+}
 
 module.exports = { connect };
